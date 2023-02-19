25281f6c7ff0bdef36f4c2f59da024ddc88e6cd0
root <antsap01@anthony.saporito@mymail.champlain.edu>
2023-02-19 00:59:52 +0000

    new secure-ssh.sh file

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -1,6 +1,14 @@
+
 #secure-ssh.sh
 #author antsap01
 #creates a new ssh user using $1 parameter
+read -p "Add user: " username
+useradd -m -d /home/"${username}" -s /bin/bash "${username}"
+mkdir /home/"${username}"/.ssh
+cp /home/anthony/anthony-sys265/SYS265/linux/public-keys/id_rsa.pub /home/"${username}"/.ssh/authorized_keys
+chmod 700 /home/"${username}"/.ssh
+chmod 600 /home/"${username}"/.ssh/authorized_keys
+chown -R "${username}":"${username}" /home/"${username}"/.ssh
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
 echo "ALL YOUR CODE GOES HERE"
